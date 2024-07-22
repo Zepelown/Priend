e6136683c3585b2d25ba7d40d247953ccc9e3b86
Zepelown <dbswjdqhd12@gmail.com>
2024-07-23 02:49:49 +0900

    add(server-stt): add google cloud stt api testing
    - Add SttController and SttService to test stt api
    - Add SttControllerTest
    
    issue #4

--- server/src/main/java/com/server/priend/stt/controller/SttController.java
+++ server/src/main/java/com/server/priend/stt/controller/SttController.java
@@ -1,8 +1,34 @@
 package com.server.priend.stt.controller;
 
+import com.server.priend.stt.domain.SttService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-@Controller
+import java.io.IOException;
+@Slf4j
+@RestController
+@RequestMapping("stt")
 public class SttController {
+    private SttService sttService;
+    public SttController(SttService sttService){
+        this.sttService = sttService;
+    }
 
+    @PostMapping(value = "/audio", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> handleAudioMessage(@RequestParam("audioFile") MultipartFile audioFile) throws IOException {
+
+        String transcribe = sttService.transcribe(audioFile);
+
+        System.out.println("transcribe"+ transcribe);
+
+        return ResponseEntity.ok().body(transcribe);
+    }
 }
